6afcb8d8697f7394a0d8e9325e8e7f74b0597936
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2025-04-10 12:38:33 -0300

    fix(exec): increase default timeout for execute and backup commands

--- types/exec.go
+++ types/exec.go
@@ -10,6 +10,7 @@ const (
 )
 
 const (
-	ExecuteNoTimeout      = time.Duration(-1)
-	ExecuteDefaultTimeout = time.Minute
+	ExecuteNoTimeout            = time.Duration(-1)
+	ExecuteDefaultTimeout       = time.Minute * 5  // Increase default timeout for execute commands to 5'
+	BackupExecuteDefaultTimeout = time.Minute * 30 // Increase default timeout for backup execute commands to 30'
 )
